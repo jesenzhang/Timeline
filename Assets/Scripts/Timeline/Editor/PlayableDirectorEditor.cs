1cec83128cb4b5493baf0cee63ee570cae87a337
jesenzhang <jesen-zh@outlook.com>
2018-04-11 16:42:31 +0800

    重命名

---
+++ Assets/Scripts/Timeline/Editor/PlayableDirectorEditor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[CustomEditor(typeof(PlayableDirector))]
+public class PlayableDirectorEditor : Editor
+{
+    
+}
